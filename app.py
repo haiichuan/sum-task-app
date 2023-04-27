e9d8afe794bb6243718f7895f7eb46c7dec70663
haiichuan <51167074+haiichuan@users.noreply.github.com>
2023-04-27 13:11:46 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+
+def find_combinations_summing_to_target(numbers, target):
+    """
+    Given a list of numbers and a target number, return a list of all combinations
+    of the numbers that sum to the target.
+    """
+
+    def backtrack(start, target, path):
+        if target == 0:
+            result.append(path)
+            return
+        for i in range(start, len(numbers)):
+            if numbers[i] > target:
+                break
+            backtrack(i + 1, target - numbers[i], path + [numbers[i]])
+
+    result = []
+    numbers.sort()
+    backtrack(0, target, [])
+    return result
+
+
+# Set up the Streamlit app
+st.title("查找数字加和组合")
+
+# Get the input from the user
+numbers = st.text_input("输入一组数字，用逗号','分隔")
+target = st.number_input("输入目标数字", format="%0.5f")
+
+click = st.columns([2, 1, 2])[1].button('计算')
+if click:
+    # Convert the input into a list of integers
+    numbers = [float(n.strip()) for n in numbers.replace('，', ',').split(",")]
+
+    # Find the combinations that sum to the target
+    combinations = find_combinations_summing_to_target(numbers, target)
+
+    # Display the results
+    if combinations:
+        st.divider()
+        st.write(f"共找到 {len(combinations)} 种计算组合的和是 {target}:")
+        with st.container():
+            for c in combinations:
+                st.success(f"{'+'.join(map(str, c))} = {target}")
+    else:
+        st.write("没有找到可以生成的组合")
